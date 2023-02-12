abbd522d364f9dee348eac8d29eb29796fbcd92e
cameronmagarin <122569279+cameronmagarin@users.noreply.github.com>
2023-02-12 15:39:48 -0500

    Add files via upload

---
+++ LAB_05_04_TheaterKiosk.java
@@ -0,0 +1,16 @@
+public class LAB_05_04_TheaterKiosk {
+    public static void main(String[] args) throws Exception {
+        String menuWristband = "Are you over 21?";
+        int initialAge;
+        int wristbandAge = 21;
+
+        //input initialAge;
+        initialAge = 13;
+       
+        if(initialAge >= wristbandAge)//checks if user age is equal to or over 21
+        {
+            System.out.println("You get a wristband!");
+        }
+
+    }
+}
